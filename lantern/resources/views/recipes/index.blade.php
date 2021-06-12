d6cccecb5e4fe6b56f9ed8baae34c6855607a388
greentomatoma <greentomatoma@gmail.com>
2021-06-12 17:10:18 +0900

    レシピ一覧表示

--- lantern/resources/views/recipes/index.blade.php
+++ lantern/resources/views/recipes/index.blade.php
@@ -8,24 +8,16 @@
 
 <div class="container">
   <div class="row">
-    <div class="card mr-5" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="img">
-      <div class="card-body">
-        <h5 class="card-title">タイトル</h5>
-        <p class="card-text">料理説明</p>
-        <a href="#" class="btn btn-primary">レシピをみる</a>
+    @foreach($recipes as $recipe)
+      <div class="card mr-5" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="img">
+        <div class="card-body">
+          <h5 class="card-title">{{ $recipe->title }}</h5>
+          <a href="#" class="btn btn-warning">レシピをみる</a>
+        </div>
       </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="img">
-      <div class="card-body">
-        <h5 class="card-title">タイトル</h5>
-        <p class="card-text">料理説明</p>
-        <a href="#" class="btn btn-primary">レシピをみる</a>
-      </div>
-    </div>
+    @endforeach
   </div>
-
 </div>
 
 @endsection
