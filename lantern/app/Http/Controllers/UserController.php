ed71e6cef3fd743303147b29b30e4a5cf7102ad9
greentomatoma <greentomatoma@gmail.com>
2021-06-23 11:20:00 +0900

    コントローラー編集

--- lantern/app/Http/Controllers/UserController.php
+++ lantern/app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -18,9 +20,23 @@ class UserController extends Controller
         ]);
     }
 
+
     public function edit(string $name, User $user)
     {
         $user = User::where('name', $name)->first();
         return view('users.edit', ['user' => $user]);
+    }
+
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user = Auth::user();
+
+        $user->name = $request->input('name');
+        $user->save();
+
+        return redirect()
+            ->route('users.show', ['name' => $user->name])
+            ->with('status', 'プロフィールを変更しました');
     }
 }
