6b8bc00ea69006600b007347b0502110678e17eb
greentomatoma <greentomatoma@gmail.com>
2021-06-27 16:24:23 +0900

    検索機能 空白を含めた検索

--- lantern/app/Http/Controllers/SearchController.php
+++ lantern/app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RecipeRequest;
 use App\Models\Recipe;
+use App\Models\Search;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -12,36 +13,46 @@ class SearchController extends Controller
     {
         $query = Recipe::query();
 
-        $keyword = '%' . $this->escape($request->input('keyword')) . '%';
+        $keyword = $this->escape($request->input('keyword'));
+        
+        $keywords = preg_split('/[\p{Z}\p{Cc}]++/u', $keyword, -1, PREG_SPLIT_NO_EMPTY);
 
-        if(!empty($keyword)) {
-            $query->where('title', 'LIKE', $keyword)
-            ->orWhere('cook_time', 'LIKE', $keyword)
-            ->orWhere('ingredients', 'LIKE', $keyword)
-            ->get();
-            $recipes = $query->get()->sortByDesc('created_at');
-        } else {
-            $recipes = Recipe::all()->sortByDesc('created_at');
-        };
-        
-        
-        // if($request->filled('keyword')) {
-        //     $query->where(function($query) use ($keyword) {
-        //     $query->where('title', 'LIKE', $keyword)->orWhere('cook_time', 'LIKE', $keyword);
-        //     });
+        // foreach ($keywords as $keyword) {
+        //     if(!empty($keyword)) {
+        //         $query->where('title', 'like', "%$keyword%")
+        //               ->orWhere('cook_time', 'like', "%$keyword%")
+        //               ->orWhere('ingredients', 'like', "%$keyword%");
+        //         // return $recipes = $query->get()->sortByDesc('created_at');
+        //     } else  {
+        //         // 検索欄に何も記入せず、検索した場合
+        //         // return $recipes = Recipe::all()->sortByDesc('created_at');
+        //     }
         // }
+
+        if(!empty($keywords)) {
+            foreach ($keywords as $keyword) {
+                $query->where('title', 'like', "%$keyword%")
+                    ->orWhere('cook_time', 'like', "%$keyword%")
+                    ->orWhere('ingredients', 'like', "%$keyword%");
+            }
+        }
+
+        $recipes = $query->get()->sortByDesc('created_at');
 
         return view('search.index', [
             'recipes' => $recipes,
-            'keyword' => $keyword,
+            'keywords' => $keywords,
         ]);
-        // return view('search.index');
-
     }
 
 
-    private function escape(string $value)
+    private function escape(string $value = null)
     {
+        // 検索欄に文字が入力された際、str_replaceを実行する
+        if(!$value) {
+            return $value;
+        }
+
         return str_replace(
             ['\\', '%', '_'],
             ['\\\\', '\\%', '\\_'],
