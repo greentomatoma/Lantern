425682edc858fe190e1be3d0ccdf2ab5a9e25af3
greentomatoma <greentomatoma@gmail.com>
2021-07-04 20:30:31 +0900

    レイアウト修正

--- lantern/resources/views/recipes/create.blade.php
+++ lantern/resources/views/recipes/create.blade.php
@@ -8,20 +8,18 @@
 @include('layouts/nav')
 
 @section('content')
-<div class="container d-flex justify-content-center mt-3">
-    <div class="card border-light" style="width: 700px">
-        <div class="row">
-        </div>
-          <div class="font-weight-bold text-center border-bottom pb-3 pt-3" style="font-size: 24px">レシピ投稿</div>
+<div class="container">
+    <div class="card border-light">
+          <h1 class="post-recipe">レシピ投稿</h1>
 
             @include('layouts/error_card_list')
 
-            <form method="POST" action="{{ route('recipes.store') }}" class="pt-5 pr-5 pb-3 pl-5" enctype="multipart/form-data">
+            <form method="POST" action="{{ route('recipes.store') }}" class="post-recipe-form" enctype="multipart/form-data">
 
                 @include('recipes/form')
 
-                <div class="form-group mt-5">
-                    <button type="submit" class="btn btn-block btn-warning p-2">
+                <div class="post-button">
+                    <button type="submit" class="btn p-2">
                         投稿
                     </button>
                 </div>
