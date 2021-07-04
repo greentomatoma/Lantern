e859110e8e449a45a24503dac245418961a64699
greentomatoma <greentomatoma@gmail.com>
2021-07-04 14:47:08 +0900

    ユーザーページ レイアウト修正

--- lantern/resources/views/users/show.blade.php
+++ lantern/resources/views/users/show.blade.php
@@ -5,66 +5,62 @@
 @include('layouts/nav')
 
 @section('content')
-<div class="container d-flex mt-4">
+<div class="user-page">
   
-    <div class="user-detail mr-3" style="width: 360px; height: 590px; background-color: white; padding: 24px;">
-      <div class="user-detail-top d-flex" style="flex-direction: column;">
+  {{-- ユーザー情報 --}}
+    <div class="user-detail">
+      <div class="user-detail-top">
         {{-- アバター画像 --}}
-          <div class="avatar-form image-picker justify-content aline-items-center">
-            <input type="file" name="avatar" class="d-none" accept="image/png,image/jpeg,image/gif" id="avatar" />
-            <label for="avatar" class="d-inline-block">
+          <div class="avatar-form image-picker">
+            <div class="avatar-image">
               @if (!empty($user->avatar_img_file))
-                <img src="/storage/avatars/{{$user->avatar_img_file}}" class="rounded-circle" style="object-fit: cover; width: 100px; height: 100px;">
+                <img src="/storage/avatars/{{$user->avatar_img_file}}" class="rounded-circle">
               @else
-                <img src="/images/avatar-default.svg" class="rounded-circle" style="object-fit: cover; width: 100px; height: 100px;">
+                <img src="/images/avatar-default.svg" class="rounded-circle">
               @endif
-            </label>
+            </div>
           </div>
     
         {{-- ニックネーム --}}
-          <div class="user-name" style="font-size: 18px;">
+          <div class="user-name">
             {{ $user->name}}
           </div>
       </div>
 
-      <div class="form-group" style="margin-top: 24px;">
-        <a href="{{ route('users.edit', ['name' => Auth::user()->name]) }}" class="btn btn-warning mt-2">
-          プロフィールを編集する
-        </a>
-      </div>
+      @if(Auth::id() === $user->id)
+        <div class="edit-profile">
+          <a href="{{ route('users.edit', ['name' => Auth::user()->name]) }}" class="edit-button">
+            プロフィールを編集する
+          </a>
+        </div>
+      @endif
     </div>
 
   {{-- 投稿レシピ一覧 --}}
-  <div class="all-post-lists" style="width: 690px; height: 1000px; background-color: white;">
-    <h2 class="list-title" style="font-size: 20px; height: 60px; background-color: white; padding-top: 16px">投稿レシピ一覧</h2>
+  <div class="all-post-lists">
+    <h2 class="list-title">投稿レシピ一覧</h2>
 
     @foreach($recipes as $recipe)
-      <div class="card" style="height: 220px; background-color: white; padding: 15px;">
+      <div class="post-recipe-card">
         <div class="card-top">
-            {{ $recipe->user->name}}さんが{{ $recipe->created_at->format('Y年m月d日') }}に投稿
-        </div>
-        <div class="card-middle d-flex">
-          <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">
-              <div class="card-img" style="width: 240px; height: 150px;">
-                  @if(!empty($recipe->cooking_img_file))
-                    <img src="/storage/recipes/{{ $recipe->cooking_img_file }}" class="card-img-top" style="width: 240px; height: 150px; object-fit: cover;">
-                  @else
-                    <img src="/images/default-recipe-image.png" class="card-img-top" style="width: 240px; height: 150px; object-fit: cover;">
-                  @endif
-              </div>
-          </a>
-          <h3 class="card-title" style="font-size: 20px;">
-            <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">{{ $recipe->title }}</a>
-          </h3>
-        </div>
-        <div class="card-bottom" style="height: 45px; ">
+            <p class="post-time">
+              <a href="#">
+              @if(!empty($user->avatar_file_name))
+                <img src="/storage/avatars/{{ $user->avatar_file_name}}" class="rounded-circle">
+              @else
+                <img src="/images/avatar-default.svg" class="rounded-circle">
+              @endif
+              {{ $recipe->user->name}}さん
+              </a> 
+              が{{ $recipe->created_at->format('Y年m月d日') }}に投稿
+            </p>
             {{-- 編集・削除 --}}
             @if(Auth::id() === $recipe->user_id)
-            <div class="d-flex aline-items-center mt-2">
-              <a class="btn mr-1" href="{{ route('recipes.edit', ['recipe' => $recipe]) }}">
+            <div class="edit-and-delete">
+              <a class="edit-btn" href="{{ route('recipes.edit', ['recipe' => $recipe]) }}">
                 <i class="fas fa-pen mt-1 fa-lg"></i>
               </a>
-              <a class="btn mr-1" data-toggle="modal" data-target="#modal-delete-{{ $recipe->id }}">
+              <a class="delete-btn" data-toggle="modal" data-target="#modal-delete-{{ $recipe->id }}">
                 <i class="fas fa-trash-alt mt-1 fa-lg"></i>
               </a>
             </div>
@@ -93,11 +89,71 @@
               </div>
             </div>
             <!-- modal -->
+            @else
+            <div class="recipe-stock mb-1">
+              <recipe-stock
+                :initial-is-stocked-by = '@json($recipe->isStockedBy(Auth::user()))'
+                :initial-count-stocks = '@json($recipe->count_stocks)'
+                :authorized = '@json(Auth::check())'
+                endpoint = "{{ route('recipes.stock', ['recipe' => $recipe]) }}"
+              >
+              </recipe-stock>
+            </div>
             @endif
+            {{-- //編集・削除 --}}
+        </div>
+
+        <div class="card-main">
+          <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">
+              <div class="post-recipe-img">
+                  @if(!empty($recipe->cooking_img_file))
+                    <img src="/storage/recipes/{{ $recipe->cooking_img_file }}" class="card-img-top">
+                  @else
+                    <img src="/images/default-recipe-image.png" class="card-img-top">
+                  @endif
+              </div>
+          </a>
+          <div class="card-main-text">
+            <h3 class="card-title">
+                <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">{{ $recipe->title }}</a>
+            </h3>
+            <div class="recipe-features">
+                <div class="body-md meal-type ">
+                  <div class="meal-type-icon"></div>
+                  <p class="meal-type">{{ $recipe->mealType->name }}</p>
+                </div>
+                <div class="body-md cook-time ">
+                  <div class="cook-time-icon"></div>
+                  <p class="cook-time">{{ $recipe->cook_time }}分</p>
+                </div>
+                <div class="body-md meal-class ">
+                  <div class="meal-class-icon"></div>
+                  <p class="meal-class">{{ $recipe->mealClass->name }}</p>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-bottom">
+          {{-- タグ --}}
+          @foreach($recipe->tags as $tag)
+            @if($loop->first)
+            <div class="tag">
+              <div class="card-text line-height">
+            @endif
+              <a class="text-muted" href="{{ route('tags.show', ['name' => $tag->name]) }}">
+                {{ $tag->hashtag }}
+              </a>
+            @if($loop->last)
+              </div>
+            </div>
+            @endif
+          @endforeach
+          {{-- //タグ --}}
         </div>
       </div>
     @endforeach
   </div>
+  
 </div>
 
 @endsection
