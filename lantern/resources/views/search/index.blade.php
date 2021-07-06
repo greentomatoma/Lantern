1da583a041283fd8df8b94e20d50f1de710ea874
greentomatoma <greentomatoma@gmail.com>
2021-07-06 23:28:46 +0900

    レイアウト修正

--- lantern/resources/views/search/index.blade.php
+++ lantern/resources/views/search/index.blade.php
@@ -8,34 +8,81 @@
 <div class="container mt-4">
 
   {{-- 検索結果一覧 --}}
-  <div class="all-post-lists" style="width: 690px; height: 1000px; background-color: white; margin: auto;">
+  <div class="all-post-lists">
 
-  <!-- {{ $searchCount }}品 -->
+  <div class="list-title">
+    <span>{{ $searchCount }}</span> 品
+  </div>
 
     @if(!empty($recipes))
+
       @foreach($recipes as $recipe)
-        <div class="card" style="height: 220px; background-color: white; padding: 15px;">
+      <div class="post-recipe-card">
           <div class="card-top">
-              {{ $recipe->user->name}}さんが{{ $recipe->created_at->format('Y年m月d日') }}に投稿
+              <p class="post-time">
+                <a href="{{ route('users.show', ['name' => $recipe->user->name] )}}">
+                @if(!empty($user->avatar_file_name))
+                  <img src="/storage/avatars/{{ $user->avatar_file_name}}" class="rounded-circle">
+                @else
+                  <img src="/images/avatar-default.svg" class="rounded-circle">
+                @endif
+                {{ $recipe->user->name}}さん
+                </a> 
+                が{{ $recipe->created_at->format('Y年m月d日') }}に投稿
+              </p>
           </div>
-          <div class="card-middle d-flex">
-          <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">
-              <div class="card-img" style="width: 240px; height: 150px;">
-                  @if(!empty($recipe->cooking_img_file))
-                    <img src="/storage/recipes/{{ $recipe->cooking_img_file }}" class="card-img-top" style="width: 240px; height: 150px; object-fit: cover;">
-                  @else
-                    <img src="/images/default-recipe-image.png" class="card-img-top" style="width: 240px; height: 150px; object-fit: cover;">
-                  @endif
+
+          <div class="card-main">
+              <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">
+                  <div class="post-recipe-img">
+                      @if(!empty($recipe->cooking_img_file))
+                        <img src="/storage/recipes/{{ $recipe->cooking_img_file }}" class="card-img-top">
+                      @else
+                        <img src="/images/default-recipe-image.png" class="card-img-top">
+                      @endif
+                  </div>
+              </a>
+              <div class="card-main-text">
+                <h3 class="card-title">
+                    <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">{{ $recipe->title }}</a>
+                </h3>
+                <div class="recipe-features">
+                    <div class="body-md meal-type ">
+                      <div class="meal-type-icon"></div>
+                      <p class="meal-type">{{ $recipe->mealType->name }}</p>
+                    </div>
+                    <div class="body-md cook-time ">
+                      <div class="cook-time-icon"></div>
+                      <p class="cook-time">{{ $recipe->cook_time }}分</p>
+                    </div>
+                    <div class="body-md meal-class ">
+                      <div class="meal-class-icon"></div>
+                      <p class="meal-class">{{ $recipe->mealClass->name }}</p>
+                    </div>
+                </div>
               </div>
-            </a>
-            <h3 class="card-title" style="font-size: 20px;">
-              <a href="{{ route('recipes.show', ['recipe' => $recipe]) }}">{{ $recipe->title }}</a>
-            </div>
-          <div class="card-bottom" style="height: 45px; ">
-              タグとブックマーク入る
           </div>
-        </div>
+
+          <div class="card-bottom">
+              {{-- タグ --}}
+              @foreach($recipe->tags as $tag)
+                @if($loop->first)
+                <div class="tag">
+                  <div class="card-text line-height">
+                @endif
+                  <a class="text-muted" href="{{ route('tags.show', ['name' => $tag->name]) }}">
+                    {{ $tag->hashtag }}
+                  </a>
+                @if($loop->last)
+                  </div>
+                </div>
+                @endif
+              @endforeach
+              {{-- //タグ --}}
+          </div>
+      </div>
       @endforeach
+      
     @else
       該当するレシピはありませんでした。
     @endif
