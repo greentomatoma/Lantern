c95955e6f64063b2a483f8227d81ed29c546612f
greentomatoma <greentomatoma@gmail.com>
2021-07-07 11:44:59 +0900

    navバー 修正

--- lantern/resources/views/layouts/nav.blade.php
+++ lantern/resources/views/layouts/nav.blade.php
@@ -4,39 +4,32 @@
           {{ config('app.name', 'Laravel') }}
       </a>
 
-      {{-- 検索窓 --}}
-        <div class="search">
-            <form class="search-form" method="GET" action="{{ route('search.index')}}">
-                <span class="fas fa-search"></span>
-                <input class="input-form" type="search" name="keyword" placeholder="食材名・料理名・調理時間 など" />
-            </form>
-        </div>
-
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Right Side Of Navbar -->
           <ul class="navbar-nav ml-auto">
-              <!-- Authentication Links -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('recipes.create') }}" >
+                    <i class="fas fa-edit"></i>
+                    <div>レシピを書く</div>
+                </a>
+            </li>
               @guest
                   {{-- 非ログイン --}}
                   <li class="nav-item">
-                      <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+                      <a class="nav-link" href="{{ route('login') }}">
+                          <i class="fas fa-sign-in-alt"></i>
+                          <div>ログイン</div>
+                      </a>
                   </li>
-                  @if (Route::has('register'))
-                      <li class="nav-item">
-                          <a class="nav-link" href="{{ route('register') }}">新規登録</a>
-                      </li>
-                  @endif
               @else
                   {{-- ログイン済み --}}
                   <li class="nav-item dropdown">
                       <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                       @if(!empty($user->avatar_file_name))
-                        <img src="/storage/avatars/{{ $user->avatar_file_name}}" class="rounded-circle" style="object-fit: cover; width: 35px; height: 35px;">
+                        <img src="/storage/avatars/{{ $user->avatar_file_name}}" class="rounded-circle">
                       @else
-                        <img src="/images/avatar-default.svg" class="rounded-circle" style="object-fit: cover; width: 35px; height: 35px;">
+                        <img src="/images/avatar-default.svg" class="rounded-circle">
                       @endif
                       </a>
-
 
                       {{-- ドロップダウンメニュー --}}
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -45,9 +38,6 @@
                           </a>
                           <a class="dropdown-item" href="{{ route('users.note', ['name' => Auth::user()->name]) }}" >
                               マイノート
-                          </a>
-                          <a class="dropdown-item" href="{{ route('recipes.create') }}" >
-                              レシピを投稿する
                           </a>
                           <a class="dropdown-item" href="{{ route('logout') }}"
                               onclick="event.preventDefault();
@@ -64,3 +54,11 @@
       </div>
   </div>
 </nav>
+
+{{-- 検索窓 --}}
+<div class="search">
+    <form class="search-form" method="GET" action="{{ route('search.index')}}">
+        <span class="fas fa-search"></span>
+        <input class="input-form" type="search" name="keyword" placeholder="食材名・料理名・調理時間 など" />
+    </form>
+</div>
