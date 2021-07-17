86b8885992e537ce03bd570f509b8ffde3bdf220
greentomatoma <greentomatoma@gmail.com>
2021-07-17 15:04:59 +0900

    SearchController リファクタリング

--- lantern/app/Http/Controllers/SearchController.php
+++ lantern/app/Http/Controllers/SearchController.php
@@ -9,41 +9,41 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
+
+    public function __construct(Search $search)
+    {
+        $this->search = $search;
+    }
+
+
     public function index(Request $request)
     {
         $query = Recipe::query();
 
+        // エスケープ処理
         $keyword = $this->escape($request->input('keyword'));
         
-        $keywords = preg_split('/[\p{Z}\p{Cc}]++/u', $keyword, -1, PREG_SPLIT_NO_EMPTY);
+        // 空白区切りで文字列を分割
+        $keywords = $this->search->pregSplit($keyword);
 
-        if(!empty($keywords)) {
-            foreach ($keywords as $keyword) {
-                $query->where('title', 'like', "%$keyword%")
-                    ->orWhere('cook_time', 'like', "%$keyword%")
-                    ->orWhere('ingredients', 'like', "%$keyword%")
-                    ->orWhereHas('tags', function($query) use ($keyword) {
-                        $query->where('name', 'like', "%$keyword%" );
-                    });
-            }
-        }
-
-        $recipes = $query->get()->sortByDesc('created_at')
-        ->load('user', 'stocks', 'tags', 'mealType', 'mealClass');
-
-        $searchCount = $query->get()->count();
+        // 検索処理
+        $recipes = $this->search->searchQuery($query, $keywords);
 
         return view('search.index', [
             'recipes' => $recipes,
             'keywords' => $keywords,
-            'searchCount' => $searchCount,
+            'searchCount' => $this->search->searchCount($recipes),
         ]);
     }
 
 
+    /**
+     * 検索欄に文字が入力された際、str_replaceを実行する
+     * @param string $value
+     * @return string $value
+     */
     private function escape(string $value = null)
     {
-        // 検索欄に文字が入力された際、str_replaceを実行する
         if(!$value) {
             return $value;
         }
--- lantern/app/Models/Search.php
+++ lantern/app/Models/Search.php
@@ -6,17 +6,50 @@ use Illuminate\Database\Eloquent\Model;
 
 class Search extends Model
 {
-    public function getSearchesCountAttribute()
+
+    /**
+     * 空白区切りで検索欄に入力された場合
+     * @param string $keyword
+     * @return Array
+     */
+    public function pregSplit($keyword)
     {
-        
+        // 全角空白→半角空白
+        $keyword = mb_convert_kana( $keyword, "s" );
+        return preg_split('/[\p{Z}\p{Cc}]++/u', $keyword, -1, PREG_SPLIT_NO_EMPTY);
     }
 
-    public function scopeSearchQuery($query, $keywords = null)
+
+    /**
+     * 検索処理
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @var string[]|false $keywords
+     * @return \Illuminate\Database\Eloquent\Builder $query
+     */
+    public function searchQuery($query, $keywords = null)
     {
-        foreach ($keywords as $keyword) {
-            $query->where('title', 'like', "%$keyword%")
-                  ->orWhere('cook_time', 'like', "%$keyword%")
-                  ->orWhere('ingredients', 'like', "%$keyword%");
+        if(!empty($keywords)) {
+            foreach ($keywords as $keyword) {
+                $query->where('title', 'like', "%$keyword%")
+                    ->orWhere('cook_time', 'like', "%$keyword%")
+                    ->orWhere('ingredients', 'like', "%$keyword%")
+                    ->orWhereHas('tags', function($query) use ($keyword) {
+                        $query->where('name', 'like', "%$keyword%" );
+                    });
+            }
         }
+        return $query->get()->sortByDesc('created_at')
+        ->load('user', 'stocks', 'tags', 'mealType', 'mealClass');
+    }
+
+
+    /**
+     * 検索ヒット数カウント
+     * @param mixed $recipes
+     * @return int
+     */
+    public function searchCount($recipes)
+    {
+        return $recipes->count();
     }
 }
