eb866667114774cdc89e7386e7f7cd8d13692b11
greentomatoma <greentomatoma@gmail.com>
2021-07-19 17:47:58 +0900

    RecipesController テスト追加

--- lantern/tests/Feature/RecipesControllerTest.php
+++ lantern/tests/Feature/RecipesControllerTest.php
@@ -26,6 +26,18 @@ class RecipesControllerTest extends TestCase
     /**
      * @test
      */
+    public function そのレシピの詳細画面表示()
+    {
+        $recipe = factory(Recipe::class)->create();
+        $response = $this->get(route('recipes.show', ['recipe' => $recipe]));
+        $response->assertStatus(200)
+            ->assertViewIs('recipes.show', ['recipe' => $recipe]);
+    }
+
+
+    /**
+     * @test
+     */
     public function 未ログインでレシピを投稿できない()
     {
         $response = $this->get(route('recipes.create'));
@@ -37,6 +49,24 @@ class RecipesControllerTest extends TestCase
      * @test
      */
     public function ログインしているとレシピを投稿できる()
+    {
+        // Userモデルを用意
+        $user = factory(User::class)->create(); 
+        
+        // ログインした上で投稿画面にアクセス
+        $response = $this->actingAs($user)
+            ->get(route('recipes.create'));
+
+        // レスポンス
+        $response->assertStatus(200)
+            ->assertViewIs('recipes.create');
+    }
+
+
+    /**
+     * @test
+     */
+    public function レシピ投稿()
     {
         // Userモデルを用意
         $user = factory(User::class)->create(); 
