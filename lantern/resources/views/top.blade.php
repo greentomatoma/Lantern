e2868d6cda114a8ab96149088099b2c2c2911e10
greentomatoma <greentomatoma@gmail.com>
2021-08-21 23:08:20 +0900

    トップページ_コード整理

--- lantern/resources/views/top.blade.php
+++ lantern/resources/views/top.blade.php
@@ -4,13 +4,13 @@
 
 <div id="main" class="top-container">
   <div class="t-wrapper">
-    <img class="t-main-top-img" src="/images/top_page/top_image.png">
+    <img class="top-img" src="/images/top_page/top_image.png">
     <div class="h1 t-main-contents">
       <span class="t-main-title">
         アイデアを
         <span class="t-main-title-orange">共有</span>しよう
       </span>
-      <span class="t-main-top-img-lantern">
+      <span class="top-img-lantern">
         <img src="/images/top_page/top_page_lantern.png">
       </span>
     </div>
@@ -18,7 +18,7 @@
 
   <div class="t-main-content">
     <div class="t-main-concept">
-      <h2 id="item1" class="concept-title">コンセプト</h2>
+      <h2 class="concept-title">コンセプト</h2>
       <div class="title-line"></div>
       <p class="concept-text">
         Lanternのコンセプトは、「<span class="text1-orange">アイデアの共有</span>」です。
@@ -35,20 +35,20 @@
       </p>
     </div>
     <div class="t-main-about">
-      <h2 class="about-title"><span id="item2" class="t-main-about-text">Lantern</span>でできること</h2>
+      <h2 class="about-title"><span class="t-main-about-text">Lantern</span>でできること</h2>
       <div class="t-items">
-        <div class="t-item box-1">
-          <p class="t-item-name"><span class="number">01</span>閲覧</p>
+        <div class="t-item">
+          <p class="t-item-title"><span class="number">01</span>閲覧</p>
           <img src="/images/top_page/top_look.png">
           <p class="t-item-text">みんなのアイデアがいつでも見れる</p>
         </div>
-        <div class="t-item box-2">
-        <p class="t-item-name"><span class="number">02</span>投稿</p>
-          <img class="img2" src="/images/top_page/top_share.png">
+        <div class="t-item">
+        <p class="t-item-title"><span class="number">02</span>投稿</p>
+          <img class="top-share" src="/images/top_page/top_share.png">
           <p class="t-item-text">アイデアを投稿しみんなで共有</p>
         </div>
-        <div class="t-item box-3">
-        <p class="t-item-name"><span class="number">03</span>保存</p>
+        <div class="t-item">
+        <p class="t-item-title"><span class="number">03</span>保存</p>
           <img src="/images/top_page/top_stock.png">
           <p class="t-item-text">お気に入りのアイデアを保存できる</p>
         </div>
