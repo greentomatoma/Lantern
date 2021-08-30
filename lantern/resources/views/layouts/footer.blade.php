3723d7a0d0ee8dbcf34cbe3a7c933fdc4c7aea74
greentomatoma <greentomatoma@gmail.com>
2021-08-30 23:27:46 +0900

    footer_レイアウト修正

--- lantern/resources/views/layouts/footer.blade.php
+++ lantern/resources/views/layouts/footer.blade.php
@@ -1,15 +1,17 @@
-<footer class="footer">
-  <div class="footer-container">
-    <div class="logo">
-        Lantern
+<footer>
+  <div class="footer">
+    <div class="footer__container">
+      <div class="footer__logo">
+          Lantern
+      </div>
+      <ul class="footer__menu">
+        <li><a class="rink" href="{{ url('/') }}">このアプリについて</a></li>
+        <li><a class="rink" href="{{ url('/about_pack') }}">災害時の調理について</a></li>
+        <li><a class="rink" href="{{ route('recipes.index') }}">レシピ一覧</a></li>
+      </ul>
     </div>
-    <ul class="nav-menu">
-      <li><a href="{{ url('/') }}">このアプリについて</a></li>
-      <li><a href="{{ url('/about_pack') }}">災害時の調理について</a></li>
-      <li><a href="{{ route('recipes.index') }}">レシピ一覧</a></li>
-    </ul>
+    <p class="footer__copyright">
+      © 2021 Lantern.
+    </p>
   </div>
-  <p class="copyright">
-    © 2021 Lantern.
-  </p>
 </footer>
