2b994a7424ad61694401f955f019654934d69f9d
greentomatoma <greentomatoma@gmail.com>
2021-08-30 22:57:13 +0900

    トップページ_レスポンシブ対応

--- lantern/resources/views/top.blade.php
+++ lantern/resources/views/top.blade.php
@@ -2,58 +2,67 @@
 
 @include('layouts/nav')
 
-<div id="main" class="top-container">
-  <div class="t-wrapper">
-    <img class="top-img" src="/images/top_page/top_image.png">
-    <div class="h1 t-main-contents">
-      <span class="t-main-title">
-        アイデアを
-        <span class="t-main-title-orange">共有</span>しよう
-      </span>
-      <span class="top-img-lantern">
-        <img src="/images/top_page/top_page_lantern.png">
-      </span>
-    </div>
+<div id="container">
+  <div id="content">
+    <main>
+        <div class="screen">
+          <div class="screen__content h1">
+            <span class="screen-title">アイデアを</span>
+            <span class="orange">共有</span>
+            <span class="screen-title">しよう</span>
+          </div>
+          <div class="screen__main">
+            <img class="main-image" src="/images/top_page/top_image.png">
+            <div class="main-lantern">
+              <img src="/images/top_page/top_page_lantern.png">
+            </div>
+          </div>
+        </div>
+    </main>
   </div>
 
-  <div class="t-main-content">
-    <div class="t-main-concept">
-      <h2 class="concept-title">コンセプト</h2>
-      <div class="title-line"></div>
-      <p class="concept-text">
-        Lanternのコンセプトは、「<span class="text1-orange">アイデアの共有</span>」です。
-        <br>
-        ランタンは、小さな灯りですがみんなで共有することができます。
-        <br>
-      </p>
-      <p class="concept-text">
-        災害時、何をどうしたら良いのかわからない...
-        <br>
-        そんな状況の中でもみんなの灯りとなれるような場を作りたいという思いから
-        <br>
-        Lanternが生まれました。
-      </p>
-    </div>
-    <div class="t-main-about">
-      <h2 class="about-title"><span class="t-main-about-text">Lantern</span>でできること</h2>
-      <div class="t-items">
-        <div class="t-item">
-          <p class="t-item-title"><span class="number">01</span>閲覧</p>
-          <img src="/images/top_page/top_look.png">
-          <p class="t-item-text">みんなのアイデアがいつでも見れる</p>
-        </div>
-        <div class="t-item">
-        <p class="t-item-title"><span class="number">02</span>投稿</p>
-          <img class="top-share" src="/images/top_page/top_share.png">
-          <p class="t-item-text">アイデアを投稿しみんなで共有</p>
-        </div>
-        <div class="t-item">
-        <p class="t-item-title"><span class="number">03</span>保存</p>
-          <img src="/images/top_page/top_stock.png">
-          <p class="t-item-text">お気に入りのアイデアを保存できる</p>
-        </div>
+  <main>
+    <section>
+      <div class="concept">
+        <h2 class="concept__title">コンセプト</h2>
+        <div class="line"></div>
+        <p class="concept__text">
+          Lanternのコンセプトは、「<span class="orange">アイデアの共有</span>」です。
+          <br>
+          ランタンは、小さな灯りですがみんなで共有することができます。
+          <br>
+        </p>
+        <p class="concept__text">
+          災害時、何をどうしたら良いのかわからない...
+          <br>
+          そんな状況の中でもみんなの灯りとなれるような場を作りたいという思いから
+          <br>
+          Lanternが生まれました。
+        </p>
       </div>
-    </div>
-  </div>
+    </section>
 
+    <section>
+      <div class="about">
+          <h2 class="about__title">できること</h2>
+          <div class="about__items">
+            <div class="about__item">
+              <p class="item-title"><span class="number">01</span>閲覧</p>
+              <img src="/images/top_page/top_look.png">
+              <p class="item-text">みんなのアイデアがいつでも見れる</p>
+            </div>
+            <div class="about__item">
+            <p class="item-title"><span class="number">02</span>投稿</p>
+              <img class="share" src="/images/top_page/top_share.png">
+              <p class="item-text">アイデアを投稿しみんなで共有</p>
+            </div>
+            <div class="about__item">
+            <p class="item-title"><span class="number">03</span>保存</p>
+              <img src="/images/top_page/top_stock.png">
+              <p class="item-text">お気に入りのアイデアを保存できる</p>
+            </div>
+          </div>
+      </div>
+    </section>
+  </main>
 </div>
